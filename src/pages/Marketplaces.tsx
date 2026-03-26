fe139cb1b413170ef84ae2f2cfeef7d82d4517d2
[dyad] <eulanybandeira@gmail.com>
2026-03-26 14:23:24 -0300

    [dyad] Correção de erro de referência do componente Label - wrote 1 file(s)

--- src/pages/Marketplaces.tsx
+++ src/pages/Marketplaces.tsx
@@ -1,6 +1,7 @@
 import { useState, useMemo } from "react";
 import { Card, CardContent, CardHeader, CardTitle, CardDescription, CardFooter } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
